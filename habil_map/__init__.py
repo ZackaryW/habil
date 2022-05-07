d838db52ce1dc47e4d405324d7214e2b534cc3eb
ZackaryW <zackaryxcwang@gmail.com>
2022-05-07 15:23:35 -0700

    struct: habil_map add init.py

---
+++ habil_map/__init__.py
@@ -0,0 +1,4 @@
+from habil_map.habiMapCase import HabiMapCase
+from habil_map.habiMapAttr import HabiMapPathParam as Path
+from habil_map.habiMapAttr import HabiMapBodyParam as Body
+from habil_map.habiMapAttr import HabiMapReturnParam as Return
