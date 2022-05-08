d20126eaa92681474f6bb8368342340458ce4cbb
ZackaryW <zackaryxcwang@gmail.com>
2022-05-07 23:57:56 -0700

    ref(HabiMapResource): strip nameing of x prefix

--- habil_map/habiMapResponse.py
+++ habil_map/habiMapResponse.py
@@ -5,87 +5,76 @@ from habil_map.habiMapAttr import HabiMapReturnParam
 
 @dataclass(frozen=True, init=False)
 class HabiMapResponse:
-    x_raw : requests.Response
-    x_is_json : bool
-    x_has_data : bool
-    x_data : typing.Dict = None
-    x_is_list : bool = False
-    x_success : bool = None
+    is_json : bool
+    json_data : typing.Optional[dict]
+    is_dict : bool
+    has_data : bool = False
+    success : bool = False
+    status_code : int = None
 
     def __init__(self, 
-        x_raw : typing.Union[requests.Response, dict],
-        x_is_json : bool = False,
-        x_has_data : bool = False, 
-        **kwargs
-
+        raw : typing.Union[requests.Response, dict],
+        ret_params : typing.Dict[str, HabiMapReturnParam] = None,
+        only_in_model : bool = True,
+        extract_data : bool = True,
     ) -> None:
-        object.__setattr__(self, "x_raw", x_raw)
-        object.__setattr__(self, "x_is_json", x_is_json)
-        object.__setattr__(self, "x_has_data", x_has_data)
-        for k, v in kwargs.items():
-            object.__setattr__(self, k, v)
+        object.__setattr__(self, "text", raw.text)
+        object.__setattr__(self, "is_dict", False)
+        object.__setattr__(self, "has_data", False)
+        object.__setattr__(self, "success", False)
+        # status code
+        object.__setattr__(self, "status_code", raw.status_code)
 
-    @property
-    def success(self) -> bool:
-        return self.x_success
-
-    @classmethod
-    def parse(cls, raw_response : requests.Response,ret_params : dict,  extract_data : bool = True, only_in_model : bool = True) -> 'HabiMapResponse':
-        #
-        if not isinstance(raw_response, requests.Response):
-            raise TypeError("raw_response must be a requests.Response")
-        #
         try:
-            json_data = raw_response.json()
-            is_json = True
+            json_data = raw.json()
+            object.__setattr__(self, "json_data", json_data.get("data", None))
+            object.__setattr__(self, "is_json", True)
+            if self.json_data is None:
+                return
+            object.__setattr__(self, "has_data", True)
+            
         except:
-            return HabiMapResponse(x_raw=raw_response)
-        #
-        if "data" not in json_data:
-            return HabiMapResponse(
-                x_raw=raw_response,
-                x_is_json=True,
-            )
-        #
-        if not isinstance(ret_params, dict):
-            ret_params = {}
+            object.__setattr__(self, "json_data", None)
+            object.__setattr__(self, "is_json", False)
+            return
 
-        # 
+        if not self.is_json:
+            return
+        
+        # check success
+        if "success" in json_data and isinstance(json_data["success"], bool) and json_data["success"]:
+            object.__setattr__(self, "success", True)
+
+        # check type
+        if isinstance(self.json_data, dict):
+            object.__setattr__(self, "is_dict", True)
+
+        if ret_params is None or not self.is_dict or len(ret_params) == 0:
+            return
+
         if not extract_data:
-            return HabiMapResponse(
-                x_raw=raw_response,
-                x_is_json=True,
-                x_has_data=True,
-                x_success=json_data.get("success", None),
-            )
-    
-        #
-        data_params : dict = json_data["data"]
-
-        if isinstance(data_params, list):
-            return HabiMapResponse(
-                x_raw=raw_response,
-                x_is_json=True,
-                x_has_data=True,
-                x_is_list=True,
-                x_data=data_params,
-                x_success=json_data.get("success", None),
-            )
-
+            return
 
         if only_in_model:
-            data_params = {k: v for k, v in data_params.items() if k in ret_params}
+            data_params = {k: v for k, v in self.json_data.items() if k in ret_params}
                 
-        kwargs = {}
         for key, val in data_params.items():
             ret : HabiMapReturnParam = ret_params[key]
             val = ret.validate(val)
-            kwargs[key] = val       
-            
-        return cls(
-            x_raw = raw_response,
-            x_data = json_data.get("data", None),
-            x_is_json = is_json,
-            x_success=json_data.get("success", None),
-            **kwargs
-        )
+            object.__setattr__(self, key, val)
+
+    @property
+    def is_list(self):
+        return not self.is_dict
+
+    @property
+    def fail(self):
+        return not self.success
+
+    @property
+    def data(self):
+        return self.json_data
+
+    @classmethod
+    def parse(cls, raw_response : requests.Response,ret_params : dict,  extract_data : bool = True, only_in_model : bool = True) -> 'HabiMapResponse':
+        return cls(raw_response, ret_params, extract_data, only_in_model)
