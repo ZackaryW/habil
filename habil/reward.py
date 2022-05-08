e4d9dab46c59009f8b603a2a93a5f43eff59afac
ZackaryW <zackaryxcwang@gmail.com>
2022-05-08 14:06:34 -0700

    feat(habil/reward): add redeem reward method

--- habil/reward.py
+++ habil/reward.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 from habil.__task__ import AHabiTask
+from habil_base.habiToken import token_required
+import habil_case
 
 @dataclass(frozen=True)
 class HabiReward(AHabiTask):
     _type = "reward"
+
+    @token_required()
+    def redeem(self,token=None):
+        return self.score_task(score=True, token=token, caller_func="habil.reward.HabiReward.redeem")
