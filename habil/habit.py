5b6e8ef44d0fb396fad3d8843d53ae9d9c5d0c1a
ZackaryW <zackaryxcwang@gmail.com>
2022-05-08 14:01:32 -0700

    misc(habil/habit): add caller_func  reference

--- habil/habit.py
+++ habil/habit.py
@@ -10,16 +10,16 @@ class HabiHabit(AHabiTask):
 
     @token_required()
     def good_habit(self,flag:bool = True, token=None):
-        return self.update(token=token,up=flag)
+        return self.update(token=token,up=flag, caller_func="habil.habit.HabiHabit.good_habit")
 
     @token_required()
     def bad_habit(self,flag:bool = True, token=None):
-        return self.update(token=token,up=flag)
+        return self.update(token=token,up=flag, caller_func="habil.habit.HabiHabit.bad_habit")
 
     @token_required()
     def score_good(self,token=None):
-        return self.score_task(score=True, token=token)
+        return self.score_task(score=True, token=token, caller_func="habil.habit.HabiHabit.score_good")
 
     @token_required()
     def score_bad(self,token=None):
-        return self.score_task(score=False, token=token)
+        return self.score_task(score=False, token=token, caller_func="habil.habit.HabiHabit.score_bad")
