f223b6a8c41d84b95831412bee5473484f5b3fe3
ZackaryW <zackaryxcwang@gmail.com>
2022-05-08 00:00:43 -0700

    fix(AHabiTask): add delete and fix token_required dec

--- habil/__task__.py
+++ habil/__task__.py
@@ -15,10 +15,17 @@ class AHabiTask(HabiUItem):
     def __str__(self) -> str:
         return "{}({})".format(self.__class__.__name__, self.id)
 
-    @token_required
+    @token_required()
     def update(self, token=None,**kwargs)-> 'AHabiTask':
         res = habil_case.task.update_a_task(headers=token, taskId=self.id, **kwargs)
         if not res.success:
-            raise Exception(res.x_raw)
-        created_obj = self.from_dict(res.x_data)
+            raise Exception(res)
+        created_obj = self.from_res(res)
         return created_obj
+
+    @token_required()
+    def delete(self, token=None) -> bool:
+        res = habil_case.task.delete_a_task(headers=token, taskId=self.id)
+        if not res.success:
+            raise Exception(res)
+        return True
