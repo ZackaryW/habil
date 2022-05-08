3739ea4733929886550169ec4b0f1d87bc5c7f86
ZackaryW <zackaryxcwang@gmail.com>
2022-05-07 21:50:47 -0700

    struct: create AHabiTask

---
+++ habil/__init__.py
@@ -0,0 +1,2 @@
+from habil.task import HabiDaily, HabiHabit, HabiReward, HabiTodo, HabiTask
+from habil_base import HabiToken, token_required
--- habil/__task__.py
+++ habil/__task__.py
@@ -1,9 +1,24 @@
 from dataclasses import dataclass
 from habil_base.habiUItem import HabiUItem
-
+import habil_case
+from habil_base import token_required
 
 @dataclass(frozen=True)
 class AHabiTask(HabiUItem):
     createdAt : str
     updatedAt : str
     text : str
+
+    def __repr__(self) -> str:
+        return "{}({})".format(self.__class__.__name__, self.text)
+
+    def __str__(self) -> str:
+        return "{}({})".format(self.__class__.__name__, self.id)
+
+    @token_required
+    def update(self, token=None,**kwargs)-> 'AHabiTask':
+        res = habil_case.task.update_a_task(headers=token, taskId=self.id, **kwargs)
+        if not res.success:
+            raise Exception(res.x_raw)
+        created_obj = self.from_dict(res.x_data)
+        return created_obj
