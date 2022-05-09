2e34979da67519f46d48496fa195c2ee464fa0fb
ZackaryW <zackaryxcwang@gmail.com>
2022-05-08 20:42:34 -0700

    fix(case): it returns id even though doc says _id

--- habil_case/user.py
+++ habil_case/user.py
@@ -4,6 +4,7 @@ login = HabiMapCase.post_case(
     "https://habitica.com/api/v3/user/auth/local/login",
     Body(name="username", xtype=str),
     Body(name="password", xtype=str),
-    Return(name="_id", xtype=str),
+    Return(name="id", xtype=str),
     Return(name="apiToken", xtype=str),
+    token_required=False
 )
