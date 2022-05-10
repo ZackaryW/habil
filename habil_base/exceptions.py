cec71ade0bd347e75e9a8b5770944bcd227bc13e
ZackaryW <zackaryxcwang@gmail.com>
2022-05-10 11:01:47 -0700

    feat(exception): adding properties

--- habil_base/exceptions.py
+++ habil_base/exceptions.py
@@ -6,7 +6,16 @@ class HabiMissingTokenException(HabiBaseException):
 
 class HabiRequestException(HabiBaseException):
     def __init__(self, res,*args) -> None:
+        self.res = res
         super().__init__(res.status_code, res.reason,*args)
+
+    @property
+    def reason(self):
+        return self.res.reason
+
+    @property
+    def code(self):
+        return self.res.code
 
 class HabiRequestRateLimited(HabiBaseException):
     pass
