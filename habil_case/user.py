c53fac225b14020678c12e91d4e9679490bd32af
ZackaryW <zackaryxcwang@gmail.com>
2022-05-10 11:00:23 -0700

    feat(case): add user cases

--- habil_case/user.py
+++ habil_case/user.py
@@ -1,5 +1,47 @@
 from habil_map import Body, Path, Return, HabiMapCase
 
+allocate_a_single_stat_point = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/allocate",
+    Path(name="stat", xtype=str, xrange=["str", "con", "int", "per"])
+)
+
+allocate_all_stat_auto = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/allocate-now"
+)
+
+# * Skip Allocate multiple Stat Points
+
+
+buy_healthy_potion = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy-health-potion"
+)
+
+buy_mystery_item_set = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy-mystery-set/{key}"
+)
+
+buy_a_piece_of_gear = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy-gear/{key}"
+)
+
+buy_a_quest_with_gold = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy-quest/{key}"
+)
+
+buy_an_enchanted_armoire_item = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy-armoire"
+)
+
+buy_item = HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy/{key}"
+)
+
+buy_special_item =HabiMapCase.post_case(
+    "https://habitica.com/api/v3/user/buy-special-spell/{key}"
+)
+
+
+
 login = HabiMapCase.post_case(
     "https://habitica.com/api/v3/user/auth/local/login",
     Body(name="username", xtype=str),
@@ -12,6 +54,16 @@ login = HabiMapCase.post_case(
 get_user_profile = HabiMapCase.get_case(
     "https://habitica.com/api/v3/user",
     Path(name="userFields", xtype=str, optional=True),
+    Return(name="stats.class", xtype=str, rename_to="stats.job", to_repo=True),
+    Return(name="stats.lvl", xtype=int, rename_to="stats.lvl", to_repo=True),
+    Return(name="stats.exp", xtype=int, rename_to="stats.exp", to_repo=True),
+    Return(name="stats.hp", xtype=int, rename_to="stats.hp", to_repo=True),
+    Return(name="stats.gp", xtype=int, rename_to="stats.gold", to_repo=True),
+    Return(name="stats.str", xtype=int, rename_to="stats.str", to_repo=True),
+    Return(name="stats.int", xtype=int, rename_to="stats.int", to_repo=True),
+    Return(name="stats.con", xtype=int, rename_to="stats.con", to_repo=True),
+    Return(name="stats.per", xtype=int, rename_to="stats.per", to_repo=True),
+    Return(name="stats.mp", xtype=int, rename_to="stats.mp", to_repo=True),
 )
 
 get_user_profile_stats = HabiMapCase.get_case(
