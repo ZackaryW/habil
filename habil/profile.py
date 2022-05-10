41da8fce712c69d6fe0e3ce518df09536fb2402d
ZackaryW <zackaryxcwang@gmail.com>
2022-05-10 11:02:40 -0700

    misc(habil/profile): subclass to HabiXItem

--- habil/profile.py
+++ habil/profile.py
@@ -1,9 +1,11 @@
 from dataclasses import dataclass
+import uuid
 from habil_base.habiToken import token_required
+from habil_base.habiXItem import HabiXItem
 import habil_case
 from habil_base.exceptions import HabiRequestException
 @dataclass(frozen=True)
-class HabiStatBox:
+class HabiStatBox(HabiXItem):
     job : str
     lvl : int
     exp : int
@@ -11,9 +13,9 @@ class HabiStatBox:
     mp : int
     gold : int
     str : int
-    int : int
     con : int
     per : int
+    int : int
 
     @classmethod
     @token_required()
@@ -21,10 +23,20 @@ class HabiStatBox:
         res = habil_case.user.get_user_profile_stats(headers=token)
         if res.fail:
             raise HabiRequestException(res)
-        return cls(**res.repo)
+        token : dict
+        return cls.from_dict(**res.repo, _raw_=res, id=token.get("x-api-user"))
 
-class HabiProfile:
+@dataclass(frozen=True)
+class HabiProfile(HabiXItem):
+    stats : HabiStatBox
+
     @classmethod
     @token_required()
     def get(cls, token=None):
-        raise NotImplementedError
+        res = habil_case.user.get_user_profile(headers=token)
+        if res.fail:
+            raise HabiRequestException(res)
+        stats = HabiStatBox.from_dict(**res.repo.get("stats"), _raw_=res, id=token.get("x-api-user"))
+        return cls.from_dict(_raw_=res, id=token.get("x-api-user"), stats=stats)
+
+    
